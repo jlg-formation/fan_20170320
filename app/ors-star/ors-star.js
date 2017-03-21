8c0d33b84d8f86f41007fad920890eea7a1ddb95
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-03-21 17:17:27 +0100

    bug: ors-star

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -3,13 +3,17 @@
 
 	var app = angular.module('ors-star', []);
 
-	app.directive('orsStar', function() {
+	app.directive('orsStar', ['$compile', function($compile) {
 		return {
 			scope: {
-				n: '=note',
+				n: '=?note',
 			},
 			link: function(scope, element, attrs) {
 				console.log('orsStar link', arguments);
+				scope.update = function(n) {
+					console.log('update', arguments);
+					scope.n = n;
+				};
 				scope.$watch('n', function() {
 					var html = '';
 					var note = 4;
@@ -18,18 +22,19 @@
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
 					for (let i = 0; i < note; i++) {
-						html += '<img src="./ors-star/img/yellow_star.png" />';
+						html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png" />';
 					}
 
 					for (let i = note; i < 5; i++) {
-						html += '<img src="./ors-star/img/white_star.png" />';
+						html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/white_star.png" />';
 					}
 					element.html(html);
+					$compile(element.contents())(scope);
 				});
 
 
 			}
 		};
-	});
+	}]);
 
 })();
