18b52d6cc1f8bfbb4c053675955b49f98bb4164c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-03-21 16:59:24 +0100

    add: ors-star

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -5,18 +5,27 @@
 
 	app.directive('orsStar', function() {
 		return {
+			scope: {
+				n: '=note',
+			},
 			link: function(scope, element, attrs) {
 				console.log('orsStar link', arguments);
-				var html = '';
-				var note = 4;
-				for (let i = 0; i < note; i++) {
-					html += '<img src="./ors-star/img/yellow_star.png" />';
-				}
+				scope.$watch('n', function() {
+					var html = '';
+					var note = 4;
+					note = (scope.n === undefined) ? note : scope.n;
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+					for (let i = 0; i < note; i++) {
+						html += '<img src="./ors-star/img/yellow_star.png" />';
+					}
 
-				for (let i = note; i < 5; i++) {
-					html += '<img src="./ors-star/img/white_star.png" />';
-				}
-				element.html(html);
+					for (let i = note; i < 5; i++) {
+						html += '<img src="./ors-star/img/white_star.png" />';
+					}
+					element.html(html);
+				});
+
 
 			}
 		};
