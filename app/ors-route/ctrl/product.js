84bfb15c928e65e98973183efa209854dd3326d0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-03-22 11:39:34 +0100

    add: product log

--- app/ors-route/ctrl/product.js
+++ app/ors-route/ctrl/product.js
@@ -10,9 +10,11 @@
 
 		ctrl.start = function() {
 			console.log('Product ctrl Start', arguments);
+			ctrl.logs = [];
 
 			$http.get('../ws/s1').then(function(response) {
 				console.log('response', response);
+				ctrl.logs.push(response.data);
 				return $q.all([
 					$http.get('../ws/s2'),
 					$http.get('../ws/s3'),
@@ -20,7 +22,12 @@
 				]);
 			}).then(function(responses) {
 				console.log('responses', responses);
+				ctrl.logs.push(responses[0].data);
+				ctrl.logs.push(responses[1].data);
+				ctrl.logs.push(responses[2].data);
 				return $http.get('../ws/s5');
+			}).then(function(response) {
+				ctrl.logs.push(response.data);
 			}).catch(function(error) {
 				console.error('error', error);
 			});
